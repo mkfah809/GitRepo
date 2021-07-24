2e45c5d8f7d39875dab313ce63dc33a8a64a3e33
mkfah809 <minakaramm1@gmail.com>
2021-07-24 12:53:25 -0400

    Assignemt2_V0.01

---
+++ src/com/coderscampus/Assign2/RunnableClass_Assign2.java
@@ -0,0 +1,15 @@
+package com.coderscampus.Assign2;
+
+public class RunnableClass_Assign2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create a random class
+		
+		// Initialize that number.
+		
+		// If condition
+	}
+
+}
